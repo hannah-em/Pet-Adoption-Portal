dffe6ef373bc0b97814c58ac5dd64ca2b1f060a0
zahrabuch <zahra.buchanan@mail.utoronto.ca>
2025-11-11 11:44:07 -0500

    pet class update to include setters and description attribute

--- src/main/java/entity/Pet.java
+++ src/main/java/entity/Pet.java
@@ -8,21 +8,30 @@ public class Pet {
     private String gender;
     private String size;
     private String contact;
+    private  String description;
 
     public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
 
     public String getType() {return type;}
+    public void setType(String type) {this.type = type;}
 
     public String getBreed() {return breed;}
+    public void setBreed(String breed) {this.breed = breed;}
 
     public int getAge() {return age;}
-
     public void setAge(int age) {this.age = age;}
 
     public String getGender() {return gender;}
+    public void setGender(String gender) {this.gender = gender;}
 
     public String getSize() {return size;}
+    public void setSize(String size) {this.size = size;}
 
     public String getContact() {return contact;}
+    public void setContact(String contact) {this.contact = contact;}
+
+    public String getDescription() {return description;}
+    public void setDescription(String description) {this.description = description;}
 
 }
