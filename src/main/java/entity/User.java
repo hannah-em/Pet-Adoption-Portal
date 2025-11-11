11dea028c9ffa60f04a3fd25d5e59f158233f51c
keikatee <keito.hara8@gmail.com>
2025-11-11 11:58:13 -0500

    User entity

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -9,7 +9,7 @@ public class User {
     private final String password;
 
     /**
-     * Creates a new user with the given non-empty name and non-empty password.
+     * Creates a simple new user with the given non-empty name and non-empty password.
      * @param name the username
      * @param password the password
      * @throws IllegalArgumentException if the password or name are empty
