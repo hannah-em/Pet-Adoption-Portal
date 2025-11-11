0e18fd1dee9b6fd9ff10a6faf3713df4b5b6edf6
hannah-em <aarfah210404@gmail.com>
2025-11-11 12:42:01 -0500

    Update BrowseFilterController.java

--- src/main/java/interface_adapter/browse_filter/BrowseFilterController.java
+++ src/main/java/interface_adapter/browse_filter/BrowseFilterController.java
@@ -1,1 +1,16 @@
+package interface_adapter.browse_filter;
 
+import use_case.browse_filter.*;
+
+public class BrowseFilterController {
+    private final BrowseFilterInputBoundary interactor;
+
+    public BrowseFilterController(BrowseFilterInputBoundary interactor) {
+        this.interactor = interactor;
+    }
+
+    public void onSearch(String species, String gender) {
+        BrowseFilterInputData inputData = new BrowseFilterInputData(species, gender);
+        interactor.execute(inputData);
+    }
+}
