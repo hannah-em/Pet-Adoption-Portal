fd627784f06a5ab605c5c02ad4b60ab14ff18f54
hannah-em <aarfah210404@gmail.com>
2025-11-12 10:55:13 -0500

    Update BrowseFilterOutputData.java

--- src/main/java/use_case/browse_filter/BrowseFilterOutputData.java
+++ src/main/java/use_case/browse_filter/BrowseFilterOutputData.java
@@ -1,14 +1,1 @@
-package use_case.browse_filter;
 
-import entity.Pet;
-import java.util.List;
-
-public class BrowseFilterOutputData {
-    private final List<Pet> pets;
-
-    public BrowseFilterOutputData(List<Pet> pets) {
-        this.pets = pets;
-    }
-
-    public List<Pet> getPets() { return pets; }
-}
