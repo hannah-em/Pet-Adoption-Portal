3e6643c4aed86af6f01e34b600c311e438dca401
hannah-em <aarfah210404@gmail.com>
2025-11-12 10:54:31 -0500

    Update BrowseFilterInputData.java

--- src/main/java/use_case/browse_filter/BrowseFilterInputData.java
+++ src/main/java/use_case/browse_filter/BrowseFilterInputData.java
@@ -1,14 +1,1 @@
-package use_case.browse_filter;
 
-public class BrowseFilterInputData {
-    private final String species;
-    private final String gender;
-
-    public BrowseFilterInputData(String species, String gender) {
-        this.species = species;
-        this.gender = gender;
-    }
-
-    public String getSpecies() { return species; }
-    public String getGender() { return gender; }
-}
