f8df1b39f319be45f6bd56caf7c2f70b12712bba
hannah-em <aarfah210404@gmail.com>
2025-11-12 10:53:36 -0500

    Update BrowseFilterViewModel.java

--- src/main/java/interface_adapter/browse_filter/BrowseFilterViewModel.java
+++ src/main/java/interface_adapter/browse_filter/BrowseFilterViewModel.java
@@ -1,19 +1,1 @@
-package interface_adapter.browse_filter;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class BrowseFilterViewModel {
-    private List<String> petsList = new ArrayList<>();
-
-    public List<String> getPetsList() {
-        return petsList;
-    }
-
-    public void setPetsList(List<String> petsList) {
-        this.petsList = petsList;
-        // In a full app, you’d notify listeners here (for now, just print)
-        System.out.println("🐾 Found pets: ");
-        petsList.forEach(System.out::println);
-    }
-}
