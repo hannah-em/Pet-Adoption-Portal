39da1076f2c9367207a0a431cbb408347a4a14b2
KaedeKagami86 <105937682+KaedeKagami86@users.noreply.github.com>
2025-11-18 20:08:53 -0500

    temporary fix for manage application db part

--- src/main/java/data_access/DatabaseApplicationGateway.java
+++ src/main/java/data_access/DatabaseApplicationGateway.java
@@ -16,33 +16,33 @@ public class DatabaseApplicationGateway implements ApplicationGatewayInterface {
         this.conn = conn;
     }
 
-    @Override
-    public Application fetchApplicationById(String applicationId) { // why does applicationID have to be a string not int?
-        String sql = "SELECT * FROM pets WHERE id = ?";
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) { // here
-            stmt.setString(1, applicationId);
-            ResultSet rs = stmt.executeQuery();
-            if (rs.next()) {
-                return new Application(
-                        rs.getString("id"),
-                        rs.getString("application_id"), //TODO: make Application entity, pass in personal_info list, not separate fields!
-                        //List<String> personal_information = getPersonalInformation(user); ..... etc.
-                        rs.getString("pet_id"),
-                        rs.getString("legal_name"),
-                        rs.getString("reason"),
-                        rs.getString("availability"),
-                        rs.getString("occupation"),
-                        rs.getString("age"),
-                        rs.getString("email"),
-                        rs.getString("phone"),
-                        rs.getString("previous_experience")
-                );
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+//    @Override
+//    public Application fetchApplicationById(String applicationId) { // why does applicationID have to be a string not int?
+//        String sql = "SELECT * FROM pets WHERE id = ?";
+//        try (PreparedStatement stmt = conn.prepareStatement(sql)) { // here
+//            stmt.setString(1, applicationId);
+//            ResultSet rs = stmt.executeQuery();
+//            if (rs.next()) {
+//                return new Application(
+////                        rs.getString("id"),
+////                        rs.getString("application_id"), //TODO: make Application entity, pass in personal_info list, not separate fields!
+//                        //List<String> personal_information = getPersonalInformation(user); ..... etc.
+////                        rs.getString("pet_id"),
+////                        rs.getString("legal_name"),
+////                        rs.getString("reason"),
+//                        rs.getString("availability"),
+//                        rs.getString("occupation"),
+//                        rs.getString("age"),
+//                        rs.getString("email"),
+//                        rs.getString("phone"),
+//                        rs.getString("previous_experience")
+//                );
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
 
     public void addApplication(Application application) {
         String sql = "INSERT OR REPLACE INTO applications (id, pet_id, legal_name, reason, availability, " +
@@ -50,16 +50,17 @@ public class DatabaseApplicationGateway implements ApplicationGatewayInterface {
         try (PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setString(1, application.getApplicationId());
             stmt.setString(2, application.getPetId());
-            stmt.setString(3, application.getUser().getLegalName());
-            stmt.setString(4, application.getReason());
-            stmt.setString(5, application.getAvailability());
-            stmt.setString(6, application.getUser().getOccupation());
-            stmt.setString(7, application.getUser().getAge());
-            stmt.setString(8, application.getUser().getEmail());
-            stmt.setString(9, application.getUser().getPhone());
-            stmt.setString(10, application.getPreviousExperience());
+            stmt.setString(3, application.getUser().getFirstName());
+            stmt.setString(4, application.getUser().getLastName());
+            stmt.setString(5, application.getReason());
+            stmt.setString(6, application.getAvailability());
+            stmt.setString(7, application.getUser().getOccupation());
+            stmt.setString(8, application.getUser().getAge());
+            stmt.setString(9, application.getUser().getEmail());
+            stmt.setString(10, application.getUser().getPhoneNum());
+            stmt.setString(11, application.getPreviousExperience());
             stmt.executeUpdate();
-            System.out.println("✅ Added application to DB: " + application.getUser().getLegalName() + "s application for"
+            System.out.println("✅ Added application to DB: " + application.getUser().getLastName() + " " + application.getUser().getLastName() + "'s application for"
                     + application.getPetId() + "ID: " + application.getApplicationId());
         } catch (SQLException e) {
             e.printStackTrace();
