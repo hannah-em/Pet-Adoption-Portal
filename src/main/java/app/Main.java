758cd05da23a340e9968610a4adcd181ce5c8ade
zahrabuch <zahra.buchanan@mail.utoronto.ca>
2025-11-20 14:16:45 -0500

    DeletePetInputBoundary

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,21 +1,21 @@
-package app;
-
-import javax.swing.*;
-
-public class Main {
-    public static void main(String[] args) {
-        AppBuilder appBuilder = new AppBuilder();
-        JFrame application = appBuilder
-                .addLoginView()
-                .addSignupView()
-                .addLoggedInView()
-                .addSignupUseCase()
-                .addLoginUseCase()
-                .addChangePasswordUseCase()
-                .build();
-
-        application.pack();
-        application.setLocationRelativeTo(null);
-        application.setVisible(true);
-    }
-}
+//package app;
+//
+//import javax.swing.*;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        AppBuilder appBuilder = new AppBuilder();
+//        JFrame application = appBuilder
+//                .addLoginView()
+//                .addSignupView()
+//                .addLoggedInView()
+//                .addSignupUseCase()
+//                .addLoginUseCase()
+//                .addChangePasswordUseCase()
+//                .build();
+//
+//        application.pack();
+//        application.setLocationRelativeTo(null);
+//        application.setVisible(true);
+//    }
+//}
---
+++ src/main/java/use_case/delete_pet/DeletePetInputBoundary.java
@@ -0,0 +1,7 @@
+package use_case.delete_pet;
+
+public interface DeletePetInputBoundary {
+
+    //Executes the Delete Pet Use case
+    void execute(DeletePetInputData deletePetInputData);
+}
