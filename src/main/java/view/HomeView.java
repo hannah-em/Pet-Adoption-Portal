fc9b30431c5435fdefc9ba9fa10f8061ea6397e5
hannah-em <aarfah210404@gmail.com>
2025-11-29 14:06:13 -0500

    Update HomeView.java

--- src/main/java/view/HomeView.java
+++ src/main/java/view/HomeView.java
@@ -8,8 +8,6 @@ import interface_adapter.logged_in.LoggedInState;
 import interface_adapter.logged_in.LoggedInViewModel;
 import interface_adapter.manage_application.ManageApplicationController;
 import interface_adapter.manage_application.ManageApplicationsPageController;
-import interface_adapter.submit_application.SubmitController;
-import interface_adapter.submit_application.SubmitPageController;
 
 import javax.swing.*;
 import java.awt.*;
@@ -21,13 +19,11 @@ public class HomeView extends JPanel implements PropertyChangeListener {
     private final LoggedInViewModel loggedInViewModel;
 
     private JButton browseButton = new JButton("Browse Pets");
-    private JButton submitApplicationButton = new JButton("Submit Application");
     private JButton changePetButton = new JButton("Add/Remove Pet");
     private JButton viewApplicationsButton = new JButton("View Applications");
 
     public HomeView(LoggedInViewModel loggedInViewModel,
                     BrowseFilterPageController browsePageController,
-                    SubmitPageController submitPageController,
                     AddPetPageController addPetPageController,
                     ManageApplicationsPageController manageAppsController) {
 
@@ -37,13 +33,11 @@ public class HomeView extends JPanel implements PropertyChangeListener {
         setLayout(new GridLayout(5, 1, 10, 10));
 
         add(browseButton);
-        add(submitApplicationButton);
         add(changePetButton);
         add(viewApplicationsButton);
 
         // Link buttons to controllers
         browseButton.addActionListener(e -> browsePageController.execute());
-        submitApplicationButton.addActionListener(e -> submitPageController.execute());
         changePetButton.addActionListener(e -> addPetPageController.execute());
         viewApplicationsButton.addActionListener(e -> manageAppsController.execute());
 
@@ -56,13 +50,11 @@ public class HomeView extends JPanel implements PropertyChangeListener {
 
         if ("administrator".equals(state.getRole())) {
             browseButton.setVisible(true);
-            submitApplicationButton.setVisible(false);
 
             changePetButton.setVisible(true);
             viewApplicationsButton.setVisible(true);
         } else {
             browseButton.setVisible(true);
-            submitApplicationButton.setVisible(true);
 
             changePetButton.setVisible(false);
             viewApplicationsButton.setVisible(false);
