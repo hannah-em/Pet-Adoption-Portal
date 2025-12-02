0c5f469d0de660e114bc2e61da2f3db116f76b03
KaedeKagami86 <105937682+KaedeKagami86@users.noreply.github.com>
2025-12-01 20:46:37 -0500

    Fixed view application wrong order issue

--- src/main/java/use_case/manage_application/ManageApplicationInputBoundary.java
+++ src/main/java/use_case/manage_application/ManageApplicationInputBoundary.java
@@ -3,8 +3,4 @@ package use_case.manage_application;
 public interface ManageApplicationInputBoundary {
 
     void execute();
-    // no input bc it's just loading all applications by default
-
-    // void switchToReviewApplicationView();
-    // TODO: used when clicking upon an application
 }
