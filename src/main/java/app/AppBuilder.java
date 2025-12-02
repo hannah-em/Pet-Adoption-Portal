919a6d9262584db5412333405816d2e62e6758cf
Haizhiling Lyu <lvhaizhiling@outlook.com>
2025-12-01 21:45:35 -0500

    change the name of our application

--- src/main/java/app/AppBuilder.java
+++ src/main/java/app/AppBuilder.java
@@ -384,7 +384,7 @@ public class AppBuilder {
     }
 
     public JFrame build() {
-        final JFrame application = new JFrame("User Login Example");
+        final JFrame application = new JFrame("User Signup here");
         application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         application.add(cardPanel);
